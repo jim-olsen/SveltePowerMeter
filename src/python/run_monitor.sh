61b5e22bd63c45b74603f5671a770a3f55a1ea73
Jim Olsen <jim.olsen@modelop.com>
2022-08-18 13:51:54 -0600

    Adding kiosk mode

--- src/python/run_monitor.sh
+++ src/python/run_monitor.sh
@@ -1,4 +1,5 @@
-cd ./SveltePowerMeter/src/python/
+cd ~/SveltePowerMeter/src/python/
 python3 server.py &> monitor.out &
 sleep 20
-chromium-browser --start-fullscreen --disable-session-crashed-bubble http://localhost:8050
+#chromium-browser --start-fullscreen --disable-session-crashed-bubble http://localhost:8050
+chromium-browser --kiosk --app-auto-launched --app=http://localhost:8050
