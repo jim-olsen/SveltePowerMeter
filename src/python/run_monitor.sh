723a97dc9330d0042dc4a72a32735a78d5393928
Jim Olsen <jim.olsen@modelop.com>
2022-10-26 11:21:38 -0600

    Adding Starlink Support

--- src/python/run_monitor.sh
+++ src/python/run_monitor.sh
@@ -1,5 +1,4 @@
-cd /home/clifwlkr/SveltePowerMeter/src/python/
-python3 server.py &> monitor.out &
+python3 /home/clifwlkr/SveltePowerMeter/src/python/server.py &> monitor.out &
 sleep 20
 #chromium-browser --start-fullscreen --disable-session-crashed-bubble http://localhost:8050
 chromium-browser --kiosk --app-auto-launched --disk-cache-dir=/dev/null --disk-cache-size=1 --app=http://localhost:8050
