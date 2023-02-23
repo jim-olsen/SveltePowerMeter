757f63e026db7db493675b37dbd71505914e3361
Jim Olsen <jim.olsen@modelop.com>
2023-02-23 13:22:43 -0700

    Changed to new format of stores using subscribe and unsubscribe to shut off data polling if no component is using them

--- src/svelte/src/App.svelte
+++ src/svelte/src/App.svelte
@@ -3,7 +3,6 @@
     import StarlinkObstructionMap from "./components/starlink/StarlinkObstructionMap.svelte";
     import StarlinkUploadDataRates from "./components/starlink/StarlinkUploadDataRates.svelte"
     import StarlinkDownloadDataRates from "./components/starlink/StarlinkDownloadDataRates.svelte"
-    import StarlinkDataFetcher from "./components/starlink/StarlinkDataFetcher.svelte";
     import StarlinkPingLatency from "./components/starlink/StarlinkPingLatency.svelte";
     import StarlinkPingDrop from "./components/starlink/StarlinkPingDrop.svelte"
     import StarlinkStatusIndicator from "./components/starlink/StarlinkStatusIndicator.svelte"
@@ -16,6 +15,12 @@
     import StarlinkAntenna from "./components/starlink/StarlinkAntenna.svelte";
     import StarlinkControls from "./components/starlink/StarlinkControls.svelte";
     import ShellyDeviceList from "./components/shelly/ShellyDeviceList.svelte";
+    import Statistics from "./components/powermeter/Statistics.svelte";
+    import CurrentValues from "./components/powermeter/CurrentValues.svelte";
+    import VoltageGraph from "./components/powermeter/VoltageGraph.svelte";
+    import SolarWattsGraph from "./components/powermeter/SolarWattsGraph.svelte";
+    import BatteryWattsGraph from "./components/powermeter/BatteryWattsGraph.svelte";
+    import LoadGraph from "./components/powermeter/LoadGraph.svelte";
 
     let dashboard = true;
     let outages = false;
@@ -23,14 +28,6 @@
     let rawData = false;
     let dishControl = false;
     let testScreen = false;
-    import Statistics from "./components/powermeter/Statistics.svelte";
-    import PowerDataFetcher from "./components/powermeter/PowerDataFetcher.svelte";
-    import CurrentValues from "./components/powermeter/CurrentValues.svelte";
-    import VoltageGraph from "./components/powermeter/VoltageGraph.svelte";
-    import SolarWattsGraph from "./components/powermeter/SolarWattsGraph.svelte";
-    import BatteryWattsGraph from "./components/powermeter/BatteryWattsGraph.svelte";
-    import LoadGraph from "./components/powermeter/LoadGraph.svelte";
-
     let innerWidth = 0;
     let outerWidth = 0
     let outerHeight = 0
@@ -73,7 +70,6 @@
     </div>
 {/if}
 {#if fullScreenPowerMeter}
-    <PowerDataFetcher/>
     <div style="display: flex; flex-flow: column;">
         {#if voltageGraph}
             <div style="display:flex; flex-flow: column;justify-content: flex-start;">
@@ -166,21 +162,19 @@
         </button>
 
     </div>
-    <StarlinkDataFetcher/>
-
     {#if dashboard}
         <div style="display:flex; flex-flow:column; justify-content: center;">
             <div style="display:flex; flex-flow: row; justify-content: space-evenly; flex-wrap: wrap;">
-                <StarlinkStatusIndicator/>
+                <StarlinkStatusIndicator />
                 <div style="display:flex; flex-flow: row; justify-content: space-evenly; gap: 50px">
                     <div style="display:flex; justify-content: center; flex-flow: column; align-items: center">
                         <span><b>Alerts</b></span>
-                        <hr style="width: 100%"/>
-                        <StarlinkAlerts/>
+                        <hr style="width: 100%" />
+                        <StarlinkAlerts />
                     </div>
                     <div style="display:flex; justify-content: flex-start; flex-flow: column; align-items: center; gap: 10px">
                         <span><b>Obstruction Map</b></span>
-                        <StarlinkObstructionMap width=200 height=200/>
+                        <StarlinkObstructionMap width=200 height=200 />
                     </div>
                 </div>
             </div>
@@ -188,41 +182,41 @@
                 <div style="display:flex; flex-flow: column; justify-content: flex-start;">
                     <span><b>Upload Speed</b></span>
                     <hr style="width: 100%"/>
-                    <StarlinkUploadDataRates chartWidth={Math.min(600, outerWidth - 100)} chartHeight=200/>
+                    <StarlinkUploadDataRates chartWidth={Math.min(600, outerWidth - 100)} chartHeight=200 />
                 </div>
                 <div style="display:flex; flex-flow: column; justify-content: flex-start;">
                     <span><b>Download Speed</b></span>
                     <hr style="width: 100%"/>
-                    <StarlinkDownloadDataRates chartWidth={Math.min(600, outerWidth - 100)} chartHeight=200/>
+                    <StarlinkDownloadDataRates chartWidth={Math.min(600, outerWidth - 100)} chartHeight=200 />
                 </div>
             </div>
             <div style="display:flex; flex-flow: row; justify-content: space-evenly; gap: 20px; flex-wrap: wrap;">
                 <div style="display:flex; flex-flow: column; justify-content: flex-start;">
                     <span><b>Ping Latency</b></span>
-                    <hr style="width: 100%"/>
-                    <StarlinkPingLatency chartWidth={Math.min(600, outerWidth - 100)} chartHeight=200/>
+                    <hr style="width: 100%" />
+                    <StarlinkPingLatency chartWidth={Math.min(600, outerWidth - 100)} chartHeight=200 />
                 </div>
                 <div style="display:flex; flex-flow: column; justify-content: flex-start;">
                     <span><b>Ping Drop</b></span>
-                    <hr style="width: 100%"/>
-                    <StarlinkPingDrop chartWidth={Math.min(600, outerWidth - 100)} chartHeight=200/>
+                    <hr style="width: 100%" />
+                    <StarlinkPingDrop chartWidth={Math.min(600, outerWidth - 100)} chartHeight=200 />
                 </div>
             </div>
             <div style="display:flex; flex-flow: row; justify-content: space-around; flex-wrap: wrap;">
-                <StarlinkFirmwareVersion/>
-                <StarlinkUpTime/>
+                <StarlinkFirmwareVersion />
+                <StarlinkUpTime />
             </div>
         </div>
     {/if}
 
     {#if outages}
         <div style="display:flex; flex-flow: column; justify-content: flex-start">
-            <StarlinkOutagesList/>
+            <StarlinkOutagesList />
             <div style="display:flex; flex-flow:row; justify-content: space-around">
-                <StarlinkOutagesChart/>
+                <StarlinkOutagesChart />
             </div>
             <div style="display:flex; flex-flow:row; justify-content: space-around">
-                <StarlinkOutageDurationChart/>
+                <StarlinkOutageDurationChart />
             </div>
 
         </div>
@@ -231,72 +225,71 @@
     {#if dishControl}
         <div style="display:flex; flex-flow: column; justify-content: space-evenly; gap: 20px;">
             <span><b>Antenna Orientation</b></span>
-            <StarlinkAntenna/>
+            <StarlinkAntenna />
             <span><b>Dishy Control</b></span>
-            <StarlinkControls/>
+            <StarlinkControls />
         </div>
     {/if}
 
     {#if rawData}
         <div style="display:flex; flex-flow: column; justify-content: flex-start">
-            <StarlinkRawData/>
+            <StarlinkRawData />
         </div>
     {/if}
 {/if}
 {#if touchScreenStarlink}
-    <StarlinkDataFetcher/>
     <div style="display:flex; flex-flow: column; justify-content: space-evenly; flex-wrap: wrap;">
         {#if touchStatus}
             <div style="display:flex; flex-flow: row; justify-content: space-evenly; flex-wrap: wrap;">
-                <StarlinkStatusIndicator/>
+                <StarlinkStatusIndicator />
                 <div style="display:flex; flex-flow: row; justify-content: space-evenly; gap: 50px">
                     <div style="display:flex; justify-content: center; flex-flow: column; align-items: center">
                         <span><b>Alerts</b></span>
                         <hr style="width: 100%"/>
-                        <StarlinkAlerts/>
+                        <StarlinkAlerts />
                     </div>
                     <div style="display:flex; justify-content: flex-start; flex-flow: column; align-items: center; gap: 10px">
                         <span><b>Obstruction Map</b></span>
-                        <StarlinkObstructionMap width=200 height=200/>
+                        <StarlinkObstructionMap width=200 height=200 />
                     </div>
                 </div>
             </div>
             <div style="display:flex; flex-flow: row; justify-content: space-around; flex-wrap: wrap;">
-                <StarlinkFirmwareVersion/>
-                <StarlinkUpTime/>
+                <StarlinkFirmwareVersion />
+                <StarlinkUpTime />
             </div>
         {/if}
         {#if touchSpeed}
             <div style="display:flex; flex-flow: column; justify-content: flex-start;">
                 <span><b>Upload Speed</b></span>
                 <hr style="width: 100%"/>
-                <StarlinkUploadDataRates chartWidth={Math.min(600, outerWidth - 100)} chartHeight=160/>
+                <StarlinkUploadDataRates chartWidth={Math.min(600, outerWidth - 100)} chartHeight=160 />
             </div>
             <div style="display:flex; flex-flow: column; justify-content: flex-start;">
                 <span><b>Download Speed</b></span>
                 <hr style="width: 100%"/>
-                <StarlinkDownloadDataRates chartWidth={Math.min(600, outerWidth - 100)} chartHeight=160/>
+                <StarlinkDownloadDataRates chartWidth={Math.min(600, outerWidth - 100)} chartHeight=160 />
             </div>
         {/if}
         {#if touchQuality}
             <div style="display:flex; flex-flow: column; justify-content: flex-start;">
                 <span><b>Ping Latency</b></span>
                 <hr style="width: 100%"/>
-                <StarlinkPingLatency chartWidth={Math.min(600, outerWidth - 100)} chartHeight=160/>
+                <StarlinkPingLatency chartWidth={Math.min(600, outerWidth - 100)} chartHeight=160 />
             </div>
             <div style="display:flex; flex-flow: column; justify-content: flex-start;">
                 <span><b>Ping Drop</b></span>
                 <hr style="width: 100%"/>
-                <StarlinkPingDrop chartWidth={Math.min(600, outerWidth - 100)} chartHeight=160/>
+                <StarlinkPingDrop chartWidth={Math.min(600, outerWidth - 100)} chartHeight=160 />
             </div>
         {/if}
         {#if touchOutages}
             <div style="display:flex; flex-flow: column; justify-content: flex-start">
                 <div style="display:flex; flex-flow:row; justify-content: space-around">
-                    <StarlinkOutagesChart chartWidth={outerWidth - 20} chartHeight=200/>
+                    <StarlinkOutagesChart chartWidth={outerWidth - 20} chartHeight=200 />
                 </div>
                 <div style="display:flex; flex-flow:row; justify-content: space-around">
-                    <StarlinkOutageDurationChart chartWidth={outerWidth - 20} chartHeight=200/>
+                    <StarlinkOutageDurationChart chartWidth={outerWidth - 20} chartHeight=200 />
                 </div>
 
             </div>
@@ -304,9 +297,9 @@
         {#if touchControl}
             <div style="display:flex; flex-flow: column; justify-content: space-evenly; gap: 20px;">
                 <span><b>Antenna Orientation</b></span>
-                <StarlinkAntenna chartHeight="250"/>
+                <StarlinkAntenna chartHeight="250" />
                 <span><b>Dishy Control</b></span>
-                <StarlinkControls/>
+                <StarlinkControls />
             </div>
         {/if}
         <div style="display:flex; flex-flow: column; justify-content: flex-end">
@@ -342,7 +335,7 @@
 {#if shellyPowerDisplay}
     <div style="display:flex; flex-flow: column; justify-content: space-between; height:100%">
         <ShellyDeviceList/>
-        <div style="display:flex; flex-flow: column; justify-content: flex-end'">
+        <div style="display:flex; flex-flow: column; justify-content: flex-end;'">
             <div style="display:flex; flex-flow:row;justify-content: space-between;">
                 <button class="tabButton"
                         on:click={()=> {shellyPowerDisplay=false;fullScreenMenu=true;}}>
--- src/svelte/src/stores.js
+++ src/svelte/src/stores.js
@@ -1,8 +1,144 @@
-import { writable } from 'svelte/store';
+import { writable, get } from 'svelte/store';
 
-export const powerGraphData = writable({})
-export const powerCurrentData = writable( {})
-export const powerStatsData = writable({})
 export const powerGraphDuration = writable(4)
-export const starlinkStatus = writable({})
-export const starlinkHistory = writable( {})
+
+/**
+ * Retrieve the graph data for values over time.  The powerGraphDuration writeable provides the number of days over
+ * which to fetch the data.
+ */
+function getPowerGraphData() {
+    fetch(`/graphData?days=${get(powerGraphDuration)}`, {
+        headers: {
+            "Accept": "application/json"
+        }
+    })
+        .then(d => d.json())
+        .then(d => {
+            powerGraphData.set(d);
+        });
+}
+
+/**
+ * Subscribable that contains the latest graph data representing values over time, based on powerGraphDuration
+ * subscribable.
+ * @type {Writable<{}>}
+ */
+export const powerGraphData = writable({}, () => {
+    let unsubscribe = powerGraphDuration.subscribe(getPowerGraphData)
+    getPowerGraphData()
+    let powerGraphInterval = setInterval(getPowerGraphData, 5000);
+    return () => {
+        unsubscribe();
+        clearInterval(powerGraphInterval);
+    };
+});
+
+/**
+ * Retrieve the current live data values related to the power system.
+ */
+function getPowerCurrentData() {
+    fetch("/currentData", {
+        headers: {
+            "Accept": "application/json"
+        }
+    })
+        .then(d => d.json())
+        .then(d => {
+            powerCurrentData.set(d);
+        });
+}
+
+/**
+ * A subscribable that contains the latest live values for power related values
+ * @type {Writable<{}>}
+ */
+export const powerCurrentData = writable( {}, () => {
+    getPowerCurrentData();
+    let powerCurrentInterval = setInterval(getPowerCurrentData, 5000);
+    return () => {
+        clearInterval(powerCurrentInterval);
+    };
+});
+
+/**
+ * Retrieve statistical data for power related values showing cumulative and average values
+ */
+function getPowerStatsData() {
+    fetch("/statsData", {
+        headers: {
+            "Accept": "application/json"
+        }
+    })
+        .then(d => d.json())
+        .then(d => {
+            powerStatsData.set(d);
+        });
+}
+
+/**
+ * A subscribable that contains the latest statistical values for power related data.
+ * @type {Writable<{}>}
+ */
+export const powerStatsData = writable({}, () => {
+    getPowerStatsData();
+    let powerStatsInterval = setInterval(getPowerStatsData, 5000);
+    return () => {
+        clearInterval(powerStatsInterval);
+    };
+});
+
+/**
+ * Returns live starlink status values as defined by the dishy component of starlink
+ */
+function getStarlinkStatus() {
+    fetch("/starlink/status/", {
+        headers: {
+            "Accept": "application/json"
+        }
+    })
+        .then(d => d.json())
+        .then(d => {
+            starlinkStatus.set(d);
+        });
+}
+
+/**
+ * A subscribable that contains the latest starlink status values as retrieved from the dishy component of starlink
+ * @type {Writable<{}>}
+ */
+export const starlinkStatus = writable({}, () => {
+    getStarlinkStatus();
+    let statusInterval = setInterval(getStarlinkStatus, 500)
+    return () => {
+        clearInterval(statusInterval)
+    }
+})
+
+/**
+ * Retrieve historical values available from the dishy component of starlink
+ */
+function getStarlinkHistory() {
+    fetch("/starlink/history/", {
+        headers: {
+            "Accept": "application/json"
+        }
+    })
+        .then(d => d.json())
+        .then(d => {
+            starlinkHistory.set(d);
+        });
+}
+
+/**
+ * A subscribable that contains the latest historical data from the dishy component of starlink
+ * @type {Writable<{}>}
+ */
+export const starlinkHistory = writable( {}, () => {
+    getStarlinkHistory();
+    let historyInterval = setInterval(getStarlinkHistory, 1000);
+    return () => {
+        clearInterval(historyInterval);
+    }
+})
+
+
