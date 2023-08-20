4dabbcdd45410d22d53c4a9986dfd9ed4eb8e09f
clifwlkr <lists@tomichicreek.com>
2023-08-20 09:15:43 -0600

    Changing text size

--- src/svelte/src/components/battery/BatteryDashboard.svelte
+++ src/svelte/src/components/battery/BatteryDashboard.svelte
@@ -14,12 +14,12 @@
     $: $batteryCurrentData, getOverallAveragePercent();
 </script>
 <div style="display:flex; flex-flow:column; justify-content: flex-start; align-items: center; width: 100%;" on:click={() => currentView.set('dashboard')}>
-    <h3>Total Percent {battery_overall_percent}%</h3>
+    <span class="mediumSmallerText">Total Percent {battery_overall_percent}%</span>
     {#each $batteryCurrentData as battery}
         <div style="display:flex; flex-flow: row; justify-content: space-around; align-items: center; width: 100%">
-            <div style="display:flex; flex-grow: 1; width: 100%;"><span>{battery.name}</span></div>
-            <div style="display:flex; flex-grow: 1; width: 100%;"><span>{battery.capacity_percent}%</span></div>
-            <div style="display:flex; flex-grow: 1; width: 100%;"><span>{battery.current}A</span></div>
+            <div style="display:flex; flex-grow: 1; width: 100%;"><span class="smallText">{battery.name}</span></div>
+            <div style="display:flex; flex-grow: 1; width: 100%;"><span class="smallText">{battery.capacity_percent}%</span></div>
+            <div style="display:flex; flex-grow: 1; width: 100%;"><span class="smallText">{battery.current}A</span></div>
             <div style="display:flex; flex-grow: 1; width: 100%;">
                 <Battery battery="{battery}"/>
             </div>
