36076e2691a6d297c8d4678466d6a503dd951228
clifwlkr <lists@tomichicreek.com>
2024-11-20 05:58:48 -0700

    Fixing for when latency data is missing

--- src/svelte/src/components/starlink/StarlinkPingLatency.svelte
+++ src/svelte/src/components/starlink/StarlinkPingLatency.svelte
@@ -13,7 +13,7 @@
     let pingLatencyChartData = [];
 
     const unsubscribeHistory = starlinkGraphHistory.subscribe(history => {
-        if (history.hasOwnProperty("ping_latency")) {
+        if (history.hasOwnProperty("ping_latency") && history.ping_latency.length > 0) {
             pingLatencyChartData = [[]];
             history.ping_latency.forEach((value, i) => {
                 pingLatencyChartData[0].unshift({"x": history.uplink_bps.length - i, "y": value});
