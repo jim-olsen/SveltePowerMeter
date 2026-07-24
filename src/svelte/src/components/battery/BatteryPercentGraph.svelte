6158b0641a10664143ecf34b751fb3bffd48f726
clifwlkr <lists@tomichicreek.com>
2026-07-24 11:57:25 -0600

    Adding solar bar graphs

---
+++ src/svelte/src/components/battery/BatteryPercentGraph.svelte
@@ -0,0 +1,29 @@
+<script>
+    import {onDestroy} from 'svelte'
+    import {batteryPercentMinMaxGraphData, batteryPercentGraphDuration} from "../../stores.svelte.js";
+    import DurationalRangeBarChart from "../d3/DurationalRangeBarChart.svelte";
+
+    export let chartWidth=800
+    export let chartHeight=300
+
+    let graphData = [];
+    const unsubscribeGraph = batteryPercentMinMaxGraphData.subscribe(data => {
+        graphData = [];
+        if (data.hasOwnProperty("time")) {
+            data.time.forEach((d, i) => {
+                graphData.push({
+                    x: d,
+                    yMin: data?.min_percent?.[i] ? data.min_percent[i] : 0,
+                    yMax: data?.max_percent?.[i] ? data.max_percent[i] : 0
+                });
+            });
+        }
+    });
+
+    onDestroy(unsubscribeGraph);
+</script>
+<div style="display:flex; flex-flow:row">
+    <DurationalRangeBarChart chartHeight={chartHeight} chartWidth={chartWidth} yAxisLabel="Battery %"
+                             dataset={graphData} duration={batteryPercentGraphDuration}
+                             minColor="#8B0000" maxColor="#7CFF9A"/>
+</div>
